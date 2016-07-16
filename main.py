2036ce44c612f4b5dc41eeb45c9fc79c833d9fc6
Cornelis Dirk Haupt <dirk.haupt@gmail.com>
2016-07-15 17:51:38 -0700

    first time saving something to file (hopefully)

--- main.py
+++ main.py
@@ -129,7 +129,9 @@ my_data = [val for val in my_data if val != '']
 
 # Recompute date count but for start_dates
 # Recompute indices for end dates since you changed the array one line up
+# Find where the dates end in each case
 end_date_inds = []
+end_of_dates_inds = []
 start_dates_counts = []
 end_date_found = False
 start_dates_count = 0
@@ -143,7 +145,8 @@ for ind in range(len(my_data)):
             end_date_found = False
             if start_dates_count > 0:
                 start_dates_counts = start_dates_counts + [start_dates_count - 1]
-                assert(len(start_dates_counts) == len(end_date_inds))
+                end_of_dates_inds = end_of_dates_inds + [ind]
+                assert(len(start_dates_counts) == len(end_date_inds) == len(end_of_dates_inds))
                 start_dates_count = 0
             continue
     # Logically if you are here elem is an integer or in date_replacements
@@ -155,78 +158,100 @@ for ind in range(len(my_data)):
         if end_date_found and (len(elem) >= 6 or elem in date_replacements):
             start_dates_count = start_dates_count + 1
 
-col_names = ['center_pt_number', 'drug', 'start_date', 'end_date', 'dose', 'reason', 'conclusion']
+
+
+#center_pt_number
+col_names = ['drug', 'end_date', 'start_date', 'dose', 'reason', 'conclusion']
 center_pt_number = []
 drug = []
-start_date = []
 end_date = []
+start_date = []
 dose = []
 reason = []
 conclusion = []
 
-row = []
 rows = [col_names]
+# add drug, end_date, start_dates, doses, reasons and conclusion columns first
 for ind in range(len(end_date_inds)):
-    elem = my_data[end_date_inds[ind]]
+    start_ind = end_date_inds[ind]
+    end_date_ind = end_date_inds[ind]
     start_date_count = start_dates_counts[ind]
+    # Find the first drug for this set
+    end_date = my_data[start_ind]
+    for i in range(1, start_date_count+1):
+        start_ind = start_ind + i
+        drug = my_data[end_date_ind - start_date_count + 1]
+        row = []
+        for j in range(start_ind, start_ind + (start_date_count*4), start_date_count):
+            row = row + [my_data[j]]
+        rows = rows + [[drug, end_date] + row]
+
+with open("D:\Home\Downloads\CC1\CONCMED1_csv.csv", "wb") as f:
+    writer = csv.writer(f)
+    writer.writerows(rows)
 
 
+# add end_date column
+# for ind in range(len(end_date_inds)):
+#     my_data[ind].insert(0,my_data[end_date_inds[ind]])
+#
+# print('')
 
 
-    if start_date_count_left <= 0:
-        print("Deleting start_date_counts " + str(start_date_counts_copy[0]))
-        del start_date_counts_copy[0]
-    my_data[ind] = my_data[ind].strip()
-    elem = my_data[ind]
-    if len(row) == 0:
-        if is_center_pt_number(elem):
-            center_pt_number = center_pt_number + elem
-        else:
-            center_pt_number = center_pt_number + ' '
-
-        start_dates_found = True
-        for i in range(ind,ind+start_date_count_left):
-            if is_date(my_data[i]):
-                start_dates_found = True
-            else:
-                start_dates_found = False
-
-        if start_dates_found:
-            # Add drugs first
-            next_ind = ind - start_date_counts_copy[0]
-            row = row + my_data[next_ind]
-            drug = drug + my_data[next_ind]
-            next_ind = ind + start_date_counts_copy[0]
-
-            if is_date(my_data[next_ind]):
-                row = row + my_data[next_ind]
-                start_date = start_date + my_data[next_ind]
-                next_ind = next_ind + start_date_counts_copy[0]
-
-                if is_date(my_data[next_ind]):
-                    row = row + my_data[next_ind]
-                    end_date = end_date + my_data[next_ind]
-                    next_ind = next_ind + start_date_counts_copy[0]
-
-                    # add rest without concrete checks...
-                    row = row + my_data[next_ind]
-                    dose = dose + my_data[next_ind]
-                    next_ind = next_ind + start_date_counts_copy[0]
-                    row = row + my_data[next_ind]
-                    reason = reason + my_data[next_ind]
-                    next_ind = next_ind + start_date_counts_copy[0]
-                    row = row + my_data[next_ind]
-                    conclusion = conclusion + my_data[next_ind]
-                    next_ind = next_ind + start_date_counts_copy[0]
-
-                    print("ROW COMPLETE: " + str(row))
-                    rows = rows + [row]
-                    row = []
-                    start_date_count_left = start_date_count_left - 1
-                else:
-                    raise AssertionError("end_date not found at index " + str(ind))
-            else:
-                raise AssertionError("start_date not found at index " + str(ind))
+    # if start_date_count_left <= 0:
+    #     print("Deleting start_date_counts " + str(start_date_counts_copy[0]))
+    #     del start_date_counts_copy[0]
+    # my_data[ind] = my_data[ind].strip()
+    # elem = my_data[ind]
+    # if len(row) == 0:
+    #     if is_center_pt_number(elem):
+    #         center_pt_number = center_pt_number + elem
+    #     else:
+    #         center_pt_number = center_pt_number + ' '
+    #
+    #     start_dates_found = True
+    #     for i in range(ind,ind+start_date_count_left):
+    #         if is_date(my_data[i]):
+    #             start_dates_found = True
+    #         else:
+    #             start_dates_found = False
+    #
+    #     if start_dates_found:
+    #         # Add drugs first
+    #         next_ind = ind - start_date_counts_copy[0]
+    #         row = row + my_data[next_ind]
+    #         drug = drug + my_data[next_ind]
+    #         next_ind = ind + start_date_counts_copy[0]
+    #
+    #         if is_date(my_data[next_ind]):
+    #             row = row + my_data[next_ind]
+    #             start_date = start_date + my_data[next_ind]
+    #             next_ind = next_ind + start_date_counts_copy[0]
+    #
+    #             if is_date(my_data[next_ind]):
+    #                 row = row + my_data[next_ind]
+    #                 end_date = end_date + my_data[next_ind]
+    #                 next_ind = next_ind + start_date_counts_copy[0]
+    #
+    #                 # add rest without concrete checks...
+    #                 row = row + my_data[next_ind]
+    #                 dose = dose + my_data[next_ind]
+    #                 next_ind = next_ind + start_date_counts_copy[0]
+    #                 row = row + my_data[next_ind]
+    #                 reason = reason + my_data[next_ind]
+    #                 next_ind = next_ind + start_date_counts_copy[0]
+    #                 row = row + my_data[next_ind]
+    #                 conclusion = conclusion + my_data[next_ind]
+    #                 next_ind = next_ind + start_date_counts_copy[0]
+    #
+    #                 print("ROW COMPLETE: " + str(row))
+    #                 rows = rows + [row]
+    #                 row = []
+    #                 start_date_count_left = start_date_count_left - 1
+    #             else:
+    #                 raise AssertionError("end_date not found at index " + str(ind))
+    #         else:
+    #             raise AssertionError("start_date not found at index " + str(ind))
 
 
 
@@ -307,31 +332,31 @@ for ind in range(len(end_date_inds)):
     #                 else:
     #                     raise AssertionError("Cannot be fixed with one concatenation")
 
-start_date_counts_copy = start_date_counts[:]
-# Use those start_date_counts to correctly split all the end_date_counts
-for ind in range(len(my_data)):
-    elem = my_data[ind]
-    elem = elem.replace(' ','')
-    if len(elem) == 6*start_date_counts_copy[0]:
-        try:
-            int_check = int(elem)
-        except ValueError:
-            continue
-        end_date_split = textwrap.wrap(elem, 6)
-        print("Deleting " + str(my_data[ind]) + " at index " + str(ind))
-        del my_data[ind]
-        print("Inserting " + str(end_date_split) + " at index " + str(ind))
-        for i in reversed(end_date_split):
-            my_data.insert(ind, i)
-        del start_date_counts_copy[0]
-
-# Remove all non-date numbers
-for ind in range(len(my_data)):
-    if (len(my_data[ind]) < 6 or len(my_data[ind]) > 6):
-        try:
-            int(my_data[ind].replace(' ', ''))
-            print("Deleting " + str(my_data[ind]) + " at index " + str(ind) + " with len " + str(len(my_data[ind])))
-            my_data[ind] = ''
-        except:
-            continue
+# start_date_counts_copy = start_date_counts[:]
+# # Use those start_date_counts to correctly split all the end_date_counts
+# for ind in range(len(my_data)):
+#     elem = my_data[ind]
+#     elem = elem.replace(' ','')
+#     if len(elem) == 6*start_date_counts_copy[0]:
+#         try:
+#             int_check = int(elem)
+#         except ValueError:
+#             continue
+#         end_date_split = textwrap.wrap(elem, 6)
+#         print("Deleting " + str(my_data[ind]) + " at index " + str(ind))
+#         del my_data[ind]
+#         print("Inserting " + str(end_date_split) + " at index " + str(ind))
+#         for i in reversed(end_date_split):
+#             my_data.insert(ind, i)
+#         del start_date_counts_copy[0]
+#
+# # Remove all non-date numbers
+# for ind in range(len(my_data)):
+#     if (len(my_data[ind]) < 6 or len(my_data[ind]) > 6):
+#         try:
+#             int(my_data[ind].replace(' ', ''))
+#             print("Deleting " + str(my_data[ind]) + " at index " + str(ind) + " with len " + str(len(my_data[ind])))
+#             my_data[ind] = ''
+#         except:
+#             continue
 
